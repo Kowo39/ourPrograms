434be9b3790442804482531db2a670c123c2883b
Kowo39 <kovo39@seznam.cz>
2015-07-02 13:03:33 +0000

    init commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+	var server = http.createServer(function (request, response) {
+	response.writeHead(200, { "Content-Type": "text/plain"});
+	response.end("Hello PeterAdrian");
+});
+
+server.listen(7000);
+
